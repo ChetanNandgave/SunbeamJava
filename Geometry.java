aa84e1f89f1b9a28cac1c6189569c3bce4954dac
Chetan Nandgave <chetanndkte@gmail.com>
2025-12-11 10:11:06 +0530

    react area peri

---
+++ Geometry.java
@@ -0,0 +1,11 @@
+
+public class Geometry {
+
+    public static double rectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    public static double rectanglePerimeter(double length, double width) {
+        return 2 * (length + width);
+    }
+}
